4ebdaae2323f451ab9380e26e183924d8499dc13
chirag <chiragsharma1676@gmail.com>
2023-08-11 10:11:43 +0530

    chnaged code

--- index.js
+++ index.js
@@ -9,82 +9,61 @@ const mytoken = "billfree";
 
 
 app.listen(8000, () => {
-    console.log("webhook is listening");
+  console.log("webhook is listening");
 });
 
 //to verify working
 app.get('/', (req, res) => {
-    res.status(200).send("api is working")
+  res.status(200).send("api is working")
 })
 
 //to verify the callback url from dashboard side -cloud api side 
 app.get("/webhook", (req, res) => {
-    let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"];
-    let token = req.query["hub.verify_token"];
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
 
-    if (mode && token) {
-        if (mode === "subscribe" && token === mytoken) {
-            res.status(200).send(challenge);
-        } else {
-            res.status(403).json({ message: "error" }
-            )
-        }
+  if (mode && token) {
+    if (mode === "subscribe" && token === mytoken) {
+      res.status(200).send(challenge);
     } else {
-        res.status(200).json({ message: "connect to whatsapp api" })
+      res.status(403).json({ message: "error" }
+      )
     }
+  } else {
+    res.status(200).json({ message: "lets use  whatsapp api" })
+  }
 })
 
-app.post("/webhook", async (req, res) => {
-    try {
-        const body_params = JSON.stringify(req.body, null, 2);
-        console.log(body_params);
-        const parsed = JSON.parse(body_params);
-        console.log(1)
-        if (parsed.entry) {
-            console.log(2);
-            console.log(parsed.entry[0].changes[0])
-            console.log(3)
-            let phone_number_id = parsed.entry[0].changes[0].value.metadata.phone_number_id;
-            console.log(phone_number_id)
-            let from = parsed.entry[0].changes[0].value.messages[0].from;
-            console.log(from)
-            let msg_body = parsed.entry[0].changes[0].value.messages[0].text.body;
-            console.log(msg_body)
-            let data = JSON.stringify({
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "918924803308",
-                "type": "text",
-                "text": {
-                  "preview_url": false,
-                  "body": "MESSAGE_CONTENT"
-                }
-              });
-              
-              let config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: 'https://graph.facebook.com/v17.0/'+phone_number_id+'/messages',
-                headers: { 
-                  'Content-Type': 'application/json', 
-                  'Authorization': 'Bearer '+token
-                },
-                data : data
-              };
-              
-              axios.request(config)
-              .then((response) => {
-                console.log(JSON.stringify(response.data));
-              })
-              .catch((error) => {
-                console.log(error);
-              });
+app.post("/webhook", (req, res) => {
+  let body_params = req.body;
 
+  console.log(JSON.stringify(body_params, null, 2));
+
+  if (body_params.object) {
+    if (body_params.entry && body_params.entry[0].changes && body_params.entry[0].changes[0].value.messages && body_params.entry[0].changes[0].value.messages[0]) {
+      let phon_no_id = body_params.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = body_params.entry[0].changes[0].value.messages[0].from;
+      let msg_body = body_params.entry[0].changes[0].value.messages[0].text.body;
+
+      axios({
+        method: 'POST',
+        url: "https://graph.facebook.com/v17.0/" + phon_no_id + "/messages?access_token=" + token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: {
+            body: "Hiiii"
+          }
+        },
+        headers: {
+          "Content-Type": "application/json"
         }
-        res.sendStatus(200)
-    } catch (error) {
-        console.error("Error:", error);
-        res.sendStatus(500);
+      });
+
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
     }
+  }
 })
