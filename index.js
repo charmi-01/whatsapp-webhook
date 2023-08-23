1f42d261e9f3af1712d217a4fb833a7be2531ff6
chirag <chiragsharma1676@gmail.com>
2023-08-23 11:45:27 +0530

    cors

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ connectToMongoDB();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin:"*"
+  }
+});
 
 
 
