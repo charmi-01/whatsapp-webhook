d522fefd2e247da720d0c6efaca2f798084e992c
chirag <chiragsharma1676@gmail.com>
2023-08-23 11:13:34 +0530

    added web socket

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ const app = express().use(body_parser.json());
 connectToMongoDB();
 
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+
 //tokens
-const token ='Bearer EAAXQdCcZBoocBO4cHJ7B3tEPbXZBrcz1XrM5DTTDNXwbMm2M4HryOrl090vgfZAY1UboEKruATSTZBTtdnqJoty3Cg7XqNzWR5jBgrs6y1xmUIVZASAInFwLBLGmIVDdRdzKZA9NvlM3c6CddXfQ1XuVOtDhnZCflowtpOGBOwtJWEI3RFDOWgtRp4hRXsB7GRXmji09VRKgflUFL5H'
+const token = 'Bearer EAAXQdCcZBoocBO4cHJ7B3tEPbXZBrcz1XrM5DTTDNXwbMm2M4HryOrl090vgfZAY1UboEKruATSTZBTtdnqJoty3Cg7XqNzWR5jBgrs6y1xmUIVZASAInFwLBLGmIVDdRdzKZA9NvlM3c6CddXfQ1XuVOtDhnZCflowtpOGBOwtJWEI3RFDOWgtRp4hRXsB7GRXmji09VRKgflUFL5H'
 const mytoken = "billfree";
 
 
@@ -43,7 +50,7 @@ app.get("/webhook", (req, res) => {
   }
 })
 
-app.post("/webhook",async (req, res) => {
+app.post("/webhook", async (req, res) => {
   let body_params = req.body;
 
   console.log(JSON.stringify(body_params, null, 2));
@@ -52,49 +59,64 @@ app.post("/webhook",async (req, res) => {
     if (body_params.entry && body_params.entry[0].changes && body_params.entry[0].changes[0].value.messages && body_params.entry[0].changes[0].value.messages[0]) {
       let phon_no_id = body_params.entry[0].changes[0].value.metadata.phone_number_id;
 
-      let sender_name=body_params.entry[0].changes[0].value.contacts[0].profile.name
-      let sender_wa_id=body_params.entry[0].changes[0].value.contacts[0].wa_id
+      let sender_name = body_params.entry[0].changes[0].value.contacts[0].profile.name
+      let sender_wa_id = body_params.entry[0].changes[0].value.contacts[0].wa_id
 
       let from = body_params.entry[0].changes[0].value.messages[0].from;
       let msg_id = body_params.entry[0].changes[0].value.messages[0].id;
       let msg_body = body_params.entry[0].changes[0].value.messages[0].text.body;
       let msg_timestamp = body_params.entry[0].changes[0].value.messages[0].timestamp;
-      let msg_type= body_params.entry[0].changes[0].value.messages[0].type;
+      let msg_type = body_params.entry[0].changes[0].value.messages[0].type;
 
 
       try {
-        const message = new Message({
-          from:from,
-          id: msg_id,
-          timestamp: msg_timestamp,
-          text: msg_body,
-          type:msg_type,
-        });
-    
-        await message.save();
-    
-        console.log("message saved sucessfully");
+        const existingMessage = await Message.findOne({ id: msg_id });
+
+        if (!existingMessage) {
+          const message = new Message({
+            from: from,
+            id: msg_id,
+            timestamp: msg_timestamp,
+            text: msg_body,
+            type: msg_type,
+          });
+
+          await message.save();
+
+          io.on('connection',socket=>{
+            socket.emit('new message recieved')
+          })
+
+          console.log("Message saved successfully");
+        } else {
+          console.log("Message with the same ID already exists");
+        }
       } catch (error) {
-        console.log("error occured");
+        console.log("Error occurred while saving message:", error);
       }
 
-      try{
-        const existingContact = await Contact.findOne({ phoneNumber:sender_wa_id  });
+
+      try {
+        const existingContact = await Contact.findOne({ phoneNumber: sender_wa_id });
 
         if (!existingContact) {
           const contact = new Contact({
-            name:sender_name,
-            phoneNumber:sender_wa_id,
+            name: sender_name,
+            phoneNumber: sender_wa_id,
           });
-      
+
+
           await contact.save();
-      
+          io.on('connection',socket=>{
+            socket.emit('new contact added')
+          })
+
           console.log("contact saved sucessfully");
-        }else{
+        } else {
           console.log("contact already there");
         }
 
-      }catch(error){
+      } catch (error) {
         console.log("error occured");
       }
 
@@ -107,7 +129,7 @@ app.post("/webhook",async (req, res) => {
       //       "body": "your msg is : "+msg_body
       //   }
       // });
-      
+
       // let config = {
       //   method: 'post',
       //   maxBodyLength: Infinity,
@@ -118,7 +140,7 @@ app.post("/webhook",async (req, res) => {
       //   },
       //   data : data
       // };
-      
+
       // axios.request(config)
       // .then((response) => {
       //   console.log(JSON.stringify(response.data));
@@ -134,6 +156,6 @@ app.post("/webhook",async (req, res) => {
   }
 })
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000...");
+server.listen(3000, () => {
+  console.log('server is listening on 3000....');
 });
